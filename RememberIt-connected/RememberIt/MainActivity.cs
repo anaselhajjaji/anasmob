85c6aa753c01482998e36e48d1d65587361ddf45
EL HAJJAJI Anas <elhajjaji.anas@gmail.com>
2017-04-13 00:25:25 +0100

    Add Facebook authentication

--- RememberIt-connected/RememberIt/MainActivity.cs
+++ RememberIt-connected/RememberIt/MainActivity.cs
@@ -1,16 +1,24 @@
-﻿using Android.App;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Json;
+
+using Android.App;
 using Android.Widget;
 using Android.OS;
-using System;
-using System.Linq;
 using Android.Content;
+
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Xamarin.Auth;
 
 namespace RememberIt
 {
     [Activity(Label = "Welcome to Remember!", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
+        // UI Scheduler
+        static readonly TaskScheduler UIScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
         // Client ID
         public static string clientId = "2a6ce4df-f26f-4864-bb28-7f806abbcc67";
         public static string commonAuthority = "https://login.windows.net/common";
@@ -37,6 +45,8 @@ namespace RememberIt
             Button loginButton = FindViewById<Button>(Resource.Id.myButton);
             loginButton.Click += delegate { StartActivity(typeof(RememberListActivity)); };
 
+
+            // ADAL Authentication
             Button adalButton = FindViewById<Button>(Resource.Id.adalButton);
             adalButton.Click += async (sender, args) =>
             {
@@ -54,6 +64,66 @@ namespace RememberIt
                     StartActivity(typeof(RememberListActivity));
                 }
             };
+
+            // Facebook Authentication
+            Button fbButton = FindViewById<Button>(Resource.Id.fbButton);
+            fbButton.Click += (sender, args) =>
+            {
+                LoginToFacebook(true);
+            };
+        }
+
+        void LoginToFacebook(bool allowCancel)
+        {
+            var auth = new OAuth2Authenticator(
+                clientId: "395508370820207", // APP ID retrieved from Facebook created Application
+                scope: "email",  // the scopes for the API
+                authorizeUrl: new Uri("https://m.facebook.com/dialog/oauth/"),  // the auth URL for the service
+                redirectUrl: new Uri("https://www.facebook.com/connect/login_success.html"));
+
+            // To allow cancel of the authentication
+            auth.AllowCancel = allowCancel;
+
+            // If authorization succeeds or is canceled, .Completed will be fired.
+            auth.Completed += (s, ee) =>
+            {
+                if (!ee.IsAuthenticated)
+                {
+                    // Not Authenticated
+                    var builder = new AlertDialog.Builder(this);
+                    builder.SetMessage("Not Authenticated");
+                    builder.SetPositiveButton("Ok", (o, e) => { });
+                    builder.Create().Show();
+                    return;
+                }
+
+                // Now that we're logged in, make a OAuth2 request to get the user's info.
+                var request = new OAuth2Request("GET", new Uri("https://graph.facebook.com/me"), null, ee.Account);
+                request.GetResponseAsync().ContinueWith(t =>
+                {
+                    if (t.IsFaulted)
+                    {
+                        // Problem in authentication
+                        var builder = new AlertDialog.Builder(this);
+                        builder.SetTitle("Error");
+                        builder.SetMessage(t.Exception.Flatten().InnerException.ToString());
+                        builder.SetPositiveButton("Ok", (o, e) => { });
+                        builder.Create().Show();
+                    }
+                    else if (!t.IsCanceled)
+                    {
+                        // Authentication succeeded, show the toast then go the main activity
+                        var obj = JsonValue.Parse(t.Result.GetResponseText());
+                        Toast.MakeText(this, string.Format("Welcome {0}.", obj["name"]), ToastLength.Long).Show();
+                        StartActivity(typeof(RememberListActivity));
+                    }
+
+
+                }, UIScheduler);
+            };
+
+            var intent = auth.GetUI(this);
+            StartActivity(intent);
         }
 
         protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
